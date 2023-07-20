aeeba96f7236ee51a4a4e18126c3e9e71e2b4734
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2023-07-20 17:17:54 -0400

    Add Play again button

--- game.js
+++ game.js
@@ -1,45 +1,19 @@
 /**
- * pseudo:
- * create a 'choices' array with values 'Rock','Paper','Scissors'
- * function getComputerChoice
- *  generate random number between 0 and 2
- *  return the random num index of the choices array
  */
 const choices = ["Rock", "Paper", "Scissors"];
-let playerScore = 0;
+let playerScore = 4;
 let computerScore = 0;
 let roundNumber = 0;
 const numberOfGamesToWin = 5;
+let gameIsOver = false;
 let = 0;
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
 }
-//console.log(getComputerChoice());
 
 /**
- * pseudo:
- * function playRound(playerSelection, computerSelection)
- *  if both parameters match(lowercase) return "It's a tie, computer also chose .."
- *  create boolean variable isWinning
- *  switch playerSelection
- *    case rock
- *      computerSelection == paper ? isWinning = false :
- *        computerSelection == scissors ? isWinning = true :
- *        break
- *    case paper
- *      computerSelection == rock ? isWinning = true :
- *        computerSelection == scissors ? isWinning = false :
- *        break
- *    case scissors
- *      computerSelection == rock ? isWinning = false :
- *        computerSelection == paper ? isWinning = true :
- *        break
- *
- *  return
- *    isWinning ? 'You Win! playerSelection beats computerSelection' :
- *      'You Lose! computerSelection beats playerSelection
  */
 const [ROCK, PAPER, SCISSORS] = ["ROCK", "PAPER", "SCISSORS"];
 
@@ -87,31 +61,8 @@ function playRound(playerSelection, computerSelection) {
   ];
 }
 
-// // test
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
-// console.log(playRound(playerSelection, computerSelection));
-// playRound(playerSelection, computerSelection)[0]
-//   ? console.log("Winning!")
-//   : console.log("Losing!");
-
 /**
- * pseudo:
- * function game(numOfRounds)
- *  create playerScore variable
- *  create computerScore variable
- *  initialize both variables to zero
- *  loop through the numOfRounds
- *    create temp variable roundResult
- *    initialize value to: playRound(playerSelection, computerSelection)
- *    log results to console roundResult[1]
- *    if roundResult[0] is not undefined
- *      if roundResult[0] is true, increment playerScore by 1
- *      else increment computerScore by 1
- *  if playerScore == computerScore ? console log game is tied:
- *    else if playerScore > computerScore ? console log you won :
- *    else console log you lose
+ * function to play n rounds and log results
  */
 function game(totalNumOfRounds = 5) {
   let playerScore = 0;
@@ -146,7 +97,6 @@ function getUserInput() {
     playerChoice = prompt("Make your choice: Rock, Paper or Scisscors");
     isValidUserInput = validateUserInput(playerChoice);
   } while (!isValidUserInput);
-  // console.log("player chose:", playerChoice, typeof playerChoice);
   return playerChoice;
 }
 
@@ -157,30 +107,39 @@ function validateUserInput(input) {
 }
 
 const resultsContainer = document.querySelector("#resultsContainer");
-const p = document.createElement("p");
+const roundResultContainer = document.createElement("p");
+const playerChoiceGroupDiv = document.querySelectorAll(".group");
+const playAgainButton = document.getElementById("play-again-btn");
 
-// game(5);
-const choicesImages = document.querySelectorAll("img");
-choicesImages.forEach((choice) => {
-  choice.addEventListener("click", (e) => {
-    let playerChoice = e.target.name;
-    let computerChoice = getComputerChoice();
-    p.textContent = playRound(playerChoice, computerChoice)[1];
-    resultsContainer.appendChild(p);
-    updateUIScoreCount();
-    checkWinner();
-  });
+playerChoiceGroupDiv.forEach((choice) => {
+  choice.addEventListener("click", (e) => handlePlayerChoiceClick(e));
 });
 
+function handlePlayerChoiceClick(e) {
+  let playerChoice = e.target.name;
+  let computerChoice = getComputerChoice();
+  roundResultContainer.textContent = playRound(playerChoice, computerChoice)[1];
+  resultsContainer.appendChild(roundResultContainer);
+  updateUIScoreCount();
+  checkWinner();
+}
+
+function toggleUserInputVisability() {
+  if (gameIsOver) {
+    playerChoiceGroupDiv.forEach((group) => {
+      group.classList.add("disable-click");
+      group.classList.remove("hover");
+    });
+  }
+}
+
+function togglePlayAgainButton() {
+  if ((playAgainButton.style.display = "none")) {
+    playAgainButton.style.display = "block";
+  } else playAgainButton.style.display = "none";
+}
+
 /**
- * problem:
- * update score count on page after a playerROund
- * pseudo:
- * function updateUIScoreCount(playerHasWon)
- *  playerSpan = queryselector(player, socre-count)
- *  playerSpan.textContent = playerScore.toLocaleString('en-US,{minimumIntegerDigits: 2, useGrouping:false})
- *  computerSpan = queryselector(computer, socre-count)
- *  computerSpan.textContent = computerScore.toLocaleString('en-US,{minimumIntegerDigits: 2, useGrouping:false})
  *
  *  TODO: add styling to score of winner
  *  if playerHasWon, winner = "player" else "computer" aka class names
@@ -193,7 +152,6 @@ function updateUIScoreCount() {
     minimumIntegerDigits: 2,
     useGrouping: false,
   });
-
   const computerSpan = document.querySelector(".computer.score-count");
   computerSpan.textContent = computerScore.toLocaleString("en-US", {
     minimumIntegerDigits: 2,
@@ -201,14 +159,9 @@ function updateUIScoreCount() {
   });
 }
 
-function resetGame() {
-  playerScore = 0;
-  computerScore = 0;
-  roundNumber = 0;
-}
-
 function checkWinner() {
   if (playerScore === 5 || computerScore === 5) {
+    gameIsOver = true;
     const gameWinnerDiv = document.createElement("div");
     gameWinnerDiv.classList.add("game-result");
     if (playerScore == 5) {
@@ -216,9 +169,27 @@ function checkWinner() {
       gameWinnerDiv.textContent = "You Win!";
     } else {
       gameWinnerDiv.classList.add("loser");
-      gameWinnerDiv.textContent = "You Lose!";
+      gameWinnerDiv.textContent = "Game Over!";
     }
     resultsContainer.appendChild(gameWinnerDiv);
+    toggleUserInputVisability();
+    togglePlayAgainButton();
     return true;
   } else return false;
+}
+
+playAgainButton.addEventListener("click", resetGame);
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  roundNumber = 0;
+  gameIsOver = false;
+  resultsContainer.childNodes.forEach((childElement) => {
+    console.log(childElement);
+    resultsContainer.remove(childElement);
+  });
+  toggleUserInputVisability();
+  togglePlayAgainButton();
+  updateUIScoreCount();
 }
