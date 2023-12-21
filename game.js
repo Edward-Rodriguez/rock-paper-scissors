dba7590957966b8b0964a760287dc0a105264c9a
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2023-12-21 12:20:42 -0500

    Refactor playerhaswon function, and add styling to player choice in resultscontainer

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 const numberOfGamesToWin = 5;
-const [ROCK, PAPER, SCISSORS] = ["ROCK", "PAPER", "SCISSORS"];
+const [ROCK, PAPER, SCISSORS] = ['ROCK', 'PAPER', 'SCISSORS'];
 let playerScore = 0;
 let computerScore = 0;
 let roundNumber = 0;
@@ -7,16 +7,16 @@ let gameIsOver = false;
 
 const images = {
   player: {
-    [ROCK]: "img/icons8-hand-rock-96-player.png",
-    [PAPER]: "img/icons8-hand-96-player.png",
-    [SCISSORS]: "img/icons8-hand-scissors-player.png",
+    [ROCK]: 'img/icons8-hand-rock-96-player.png',
+    [PAPER]: 'img/icons8-hand-96-player.png',
+    [SCISSORS]: 'img/icons8-hand-scissors-player.png',
   },
   computer: {
-    [ROCK]: "img/icons8-hand-rock-96-computer.png",
-    [PAPER]: "img/icons8-hand-96-computer.png",
-    [SCISSORS]: "img/icons8-hand-scissors-computer.png",
+    [ROCK]: 'img/icons8-hand-rock-96-computer.png',
+    [PAPER]: 'img/icons8-hand-96-computer.png',
+    [SCISSORS]: 'img/icons8-hand-scissors-computer.png',
   },
-  unknown: "img/question-mark.png",
+  unknown: 'img/question-mark.png',
 };
 
 function getComputerChoice() {
@@ -31,74 +31,96 @@ function playRound(playerSelection, computerSelection) {
 
   let playerHasWon; //flag denoting player winning hand
 
-  if (playerSelection === computerSelection)
-    return [
-      playerHasWon,
-      `Round ${roundNumber}: It's a tie, computer also chose ${computerSelection}`,
-    ];
+  if (playerSelection !== computerSelection) {
+    switch (playerSelection) {
+      case ROCK:
+        computerSelection == SCISSORS
+          ? (playerHasWon = true)
+          : (playerHasWon = false);
+        break;
+      case PAPER:
+        computerSelection == ROCK
+          ? (playerHasWon = true)
+          : (playerHasWon = false);
+        break;
+      case SCISSORS:
+        computerSelection == PAPER
+          ? (playerHasWon = true)
+          : (playerHasWon = false);
+        break;
+    }
 
-  switch (playerSelection) {
-    case ROCK:
-      computerSelection == SCISSORS
-        ? (playerHasWon = true)
-        : (playerHasWon = false);
-      break;
-    case PAPER:
-      computerSelection == ROCK
-        ? (playerHasWon = true)
-        : (playerHasWon = false);
-      break;
-    case SCISSORS:
-      computerSelection == PAPER
-        ? (playerHasWon = true)
-        : (playerHasWon = false);
-      break;
-  }
-
-  if (playerHasWon !== undefined) {
     if (playerHasWon) playerScore += 1;
     else computerScore += 1;
   }
 
-  return [
+  return {
     playerHasWon,
-    playerHasWon
-      ? `Round ${roundNumber}: You Win! ${playerSelection} beats ${computerSelection}`
-      : `Round ${roundNumber}: You Lose! ${computerSelection} beats ${playerSelection}`,
-  ];
+    roundNumber,
+    playerSelection,
+    computerSelection,
+  };
 }
 
-const resultsContainer = document.querySelector("#resultsContainer");
-const roundResultContainer = document.createElement("p");
-const playerChoiceGroupDiv = document.querySelectorAll(".group");
-const playAgainButton = document.getElementById("play-again-btn");
-const gameWinnerDiv = document.createElement("div");
-const playerChoiceImage = document.querySelector(".player-choice > img");
-const computerChoiceImage = document.querySelector(".computer-choice > img");
+const resultsContainer = document.querySelector('#resultsContainer');
+const resultPlayerChoice = document.createElement('span');
+const resultComputerChoice = document.createElement('span');
+const roundResultContainer = document.createElement('p');
+const playerChoiceGroupDiv = document.querySelectorAll('.group');
+const playAgainButton = document.getElementById('play-again-btn');
+const gameWinnerDiv = document.createElement('div');
+const playerChoiceImage = document.querySelector('.player-choice > img');
+const computerChoiceImage = document.querySelector('.computer-choice > img');
 resultsContainer.appendChild(roundResultContainer);
 resultsContainer.appendChild(gameWinnerDiv);
+resultPlayerChoice.setAttribute('id', 'result-player-choice');
+resultComputerChoice.setAttribute('id', 'result-computer-choice');
 
 function handlePlayerChoiceClick(e) {
   let playerChoice = e.target.name;
   let computerChoice = getComputerChoice();
-  roundResultContainer.textContent = playRound(playerChoice, computerChoice)[1];
+  const roundResult = playRound(playerChoice, computerChoice);
+  resultPlayerChoice.textContent = roundResult.playerSelection;
+  resultComputerChoice.textContent = roundResult.computerSelection;
+  roundResultContainer.textContent = '';
+  roundResultContainer.append(`Round ${roundResult.roundNumber}:`);
+  if (roundResult.playerHasWon) {
+    roundResultContainer.append(
+      ' You Win! ',
+      resultPlayerChoice,
+      ' beats ',
+      resultComputerChoice,
+    );
+  } else if (roundResult.playerHasWon === false) {
+    roundResultContainer.append(
+      ' You Lose! ',
+      resultComputerChoice,
+      ' beats ',
+      resultPlayerChoice,
+    );
+  } else {
+    roundResultContainer.append(
+      " It's a tie, Computer also chose ",
+      resultComputerChoice,
+    );
+  }
   updateUIScoreCount();
   toggleImages(playerChoice, computerChoice);
   checkWinner();
 }
 
 playerChoiceGroupDiv.forEach((choice) => {
-  choice.addEventListener("click", (e) => handlePlayerChoiceClick(e));
+  choice.addEventListener('click', (e) => handlePlayerChoiceClick(e));
 });
 
 function updateUIScoreCount() {
-  const playerSpan = document.querySelector(".player.score-count");
-  playerSpan.textContent = playerScore.toLocaleString("en-US", {
+  const playerSpan = document.querySelector('.player.score-count');
+  playerSpan.textContent = playerScore.toLocaleString('en-US', {
     minimumIntegerDigits: 2,
     useGrouping: false,
   });
-  const computerSpan = document.querySelector(".computer.score-count");
-  computerSpan.textContent = computerScore.toLocaleString("en-US", {
+  const computerSpan = document.querySelector('.computer.score-count');
+  computerSpan.textContent = computerScore.toLocaleString('en-US', {
     minimumIntegerDigits: 2,
     useGrouping: false,
   });
@@ -107,13 +129,13 @@ function updateUIScoreCount() {
 function checkWinner() {
   if (playerScore === 5 || computerScore === 5) {
     gameIsOver = true;
-    gameWinnerDiv.classList.add("game-result");
+    gameWinnerDiv.classList.add('game-result');
     if (playerScore == 5) {
-      gameWinnerDiv.classList.add("winner");
-      gameWinnerDiv.textContent = "You Win!";
+      gameWinnerDiv.classList.add('winner');
+      gameWinnerDiv.textContent = 'You Win!';
     } else {
-      gameWinnerDiv.classList.add("loser");
-      gameWinnerDiv.textContent = "Game Over!";
+      gameWinnerDiv.classList.add('loser');
+      gameWinnerDiv.textContent = 'Game Over!';
     }
     toggleUserInputVisability();
     togglePlayAgainButton();
@@ -123,33 +145,33 @@ function checkWinner() {
 
 function toggleImages(playerChoice, computerChoice) {
   if (gameIsOver) {
-    playerChoiceImage.setAttribute("src", images.unknown);
-    computerChoiceImage.setAttribute("src", images.unknown);
+    playerChoiceImage.setAttribute('src', images.unknown);
+    computerChoiceImage.setAttribute('src', images.unknown);
   } else {
-    playerChoiceImage.setAttribute("src", images.player[playerChoice]);
-    computerChoiceImage.setAttribute("src", images.computer[computerChoice]);
+    playerChoiceImage.setAttribute('src', images.player[playerChoice]);
+    computerChoiceImage.setAttribute('src', images.computer[computerChoice]);
   }
 }
 
 function toggleUserInputVisability() {
   playerChoiceGroupDiv.forEach((group) => {
     if (gameIsOver) {
-      group.classList.add("disable-click");
-      group.classList.remove("hover");
+      group.classList.add('disable-click');
+      group.classList.remove('hover');
     } else {
-      group.classList.remove("disable-click");
-      group.classList.add("hover");
+      group.classList.remove('disable-click');
+      group.classList.add('hover');
     }
   });
 }
 
 function togglePlayAgainButton() {
-  if (playAgainButton.style.display === "block") {
-    playAgainButton.style.display = "none";
-  } else playAgainButton.style.display = "block";
+  if (playAgainButton.style.display === 'block') {
+    playAgainButton.style.display = 'none';
+  } else playAgainButton.style.display = 'block';
 }
 
-playAgainButton.addEventListener("click", resetGame);
+playAgainButton.addEventListener('click', resetGame);
 
 function resetGame() {
   toggleImages();
@@ -158,8 +180,8 @@ function resetGame() {
   roundNumber = 0;
   gameIsOver = false;
   resultsContainer.childNodes.forEach((childElement) => {
-    childElement.textContent = "";
-    childElement.classList.remove("winner", "loser");
+    childElement.textContent = '';
+    childElement.classList.remove('winner', 'loser');
   });
   toggleUserInputVisability();
   togglePlayAgainButton();
